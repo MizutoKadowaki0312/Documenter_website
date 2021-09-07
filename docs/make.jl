5537a2727f33819b88abc88ee475bd45db159948
Kadowaki0312.res@gmail.com <kadowaki0312.res@gmail.com>
2021-09-07 16:19:31 +0900

    deploydocs を追加

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,10 @@ using Documenter
 makedocs(;
     sitename="Mizuto Kadowaki"
 )
+
+deploydocs(;
+    devbranch="main",
+    target="build",
+    repo="github.com/MizutoKadowaki0312/Documenter_website",
+    versions=nothing
+)
